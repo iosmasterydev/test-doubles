6ab81b8e63db35cacf2d3ebf329c8692f4d293ff
Ibrahima Ciss <bionik6@gmail.com>
2023-01-04 03:34:01 +0000

    Initial Commit

---
+++ TestDoubles/TestDoublesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDoublesApp.swift
+//  TestDoubles
+//
+//  Created by Ibrahima Ciss on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestDoublesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
