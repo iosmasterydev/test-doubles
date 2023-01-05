562ce7a1aa1052539f44872c4a066e035d57a877
Ibrahima Ciss <bionik6@gmail.com>
2023-01-05 01:28:29 +0000

    Initial commit

--- TestDoubles/ContentView.swift
+++ TestDoubles/ContentView.swift
@@ -1,26 +1,59 @@
-//
-//  ContentView.swift
-//  TestDoubles
-//
-//  Created by Ibrahima Ciss on 04/01/2023.
-//
+import Combine
+import Foundation
 
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct Token: Decodable {
+  let value: String
+  let expiryDate: Date
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct LoginPayload: Encodable {
+  let email: String
+  let password: String
+}
+
+
+final class LoginViewModel: ObservableObject {
+  @Published var token: Token?
+  @Published var error: Error?
+  @Published var email: String = ""
+  @Published var password: String = ""
+  
+  private let loginService: LoginService
+  
+  init(loginService: LoginService = DefaultLoginService()) {
+    self.loginService = loginService
+  }
+  
+  @MainActor
+  func login() async {
+    do {
+      token = try await loginService.attemptLogin(
+        email: email,
+        password: password
+      )
+    } catch {
+      self.error = error
     }
+  }
+  
+}
+
+
+protocol LoginService {
+  func attemptLogin(email: String, password: String) async throws -> Token
+}
+
+struct DefaultLoginService: LoginService {
+  
+  func attemptLogin(email: String, password: String) async throws -> Token {
+    var request = URLRequest(url: URL(string: "any-url")!)
+    let payload = LoginPayload(email: email, password: password)
+    request.httpBody = try? JSONEncoder().encode(payload)
+    let (data, _) = try await URLSession.shared.data(for: request)
+    guard let token = try? JSONDecoder().decode(Token.self, from: data) else {
+      throw NSError(domain: "decoding-error", code: 1)
+    }
+    return token
+  }
+  
 }
---
+++ TestDoublesTests/LoginViewModelTests.swift
@@ -0,0 +1,125 @@
+import XCTest
+@testable import TestDoubles
+
+
+final class LoginViewModelTests: XCTestCase {
+  
+  func test_error_is_not_nil_if_service_sends_a_network_error() async {
+    let sut = LoginViewModel(loginService: LoginServiceSadPathStub())
+    
+    await sut.login()
+    
+    XCTAssertNotNil(sut.error)
+  }
+  
+  func test_token_is_not_nil_when_the_api_sent_a_successful_response() async {
+    let sut = LoginViewModel(loginService: LoginServiceHappyPathStub())
+    
+    await sut.login()
+    
+    XCTAssertNotNil(sut.token)
+    XCTAssertEqual(sut.token?.value, "jwt-token")
+  }
+  
+  func test_attemptLogin_is_only_called_once() async {
+    let mock = LoginServiceHappyPathMock()
+    let sut = LoginViewModel(loginService: mock)
+    
+    await sut.login()
+    
+    XCTAssertEqual(mock.loginCalled, 1)
+  }
+  
+  func test_attemptLogin_throws_an_error_when_service_sends_network_error() async {
+    let mock = LoginServiceSadPathMock()
+    let sut = LoginViewModel(loginService: mock)
+    
+    await sut.login()
+    
+    XCTAssertEqual(mock.loginCalled, 1)
+    XCTAssertNotNil(mock.error)
+  }
+  
+  func test_right_arguments_are_passed() async {
+    let spy = LoginServiceSpy()
+    let sut = LoginViewModel(loginService: spy)
+    sut.email = "hello@gmail.com"
+    sut.password = "password"
+    
+    await sut.login()
+    
+    XCTAssertEqual(spy.loginCalled, 1)
+    XCTAssertEqual(spy.receivedInvocations.count, 1)
+    XCTAssertEqual(spy.receivedArguments?.email, "hello@gmail.com")
+    XCTAssertEqual(spy.receivedArguments?.password, "password")
+  }
+  
+}
+
+
+  // MARK: - Stubs
+class LoginServiceHappyPathStub: LoginService {
+  func attemptLogin(email: String, password: String) async throws -> Token {
+    return Token(
+      value: "jwt-token",
+      expiryDate: .now.addingTimeInterval(60 * 30)
+    )
+  }
+}
+
+class LoginServiceSadPathStub: LoginService {
+  func attemptLogin(email: String, password: String) async throws -> Token {
+    throw NSError(domain: "error", code: 0)
+  }
+}
+
+
+  // MARK: - Mocks
+private class LoginServiceHappyPathMock: LoginService {
+  var error: NSError?
+  var loginCalled: Int = 0
+  
+  func attemptLogin(email: String, password: String) async throws -> Token {
+    loginCalled += 1
+    return Token(
+      value: "jwt-token",
+      expiryDate: .now.addingTimeInterval(60 * 30)
+    )
+  }
+  
+}
+
+
+class LoginServiceSadPathMock: LoginService {
+  var error: NSError?
+  var loginCalled: Int = 0
+  
+  func attemptLogin(email: String, password: String) async throws -> Token {
+    loginCalled += 1
+    let error = NSError(domain: "error", code: 0)
+    self.error = error
+    throw error
+  }
+  
+}
+
+
+// MARK: - Spies
+class LoginServiceSpy: LoginService {
+  var error: Error?
+  var loginCalled: Int = 0
+  var receivedArguments: (email: String, password: String)?
+  var receivedInvocations: [(email: String, password: String)] = []
+  
+  func attemptLogin(email: String, password: String) async throws -> Token {
+    loginCalled += 1
+    if let error { throw error }
+    receivedArguments = (email: email, password: password)
+    receivedInvocations.append((email: email, password: password))
+    return Token(
+      value: "jwt-token",
+      expiryDate: .now.addingTimeInterval(60 * 30)
+    )
+  }
+  
+}
